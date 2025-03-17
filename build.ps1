7c5b5b4a29c4d97fce33af1f86443d13c585e8e2
Björn Sundling <bjorn.sundling@gmail.com>
2025-03-18 00:04:14 +0100

    First pipeline (#5)

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 [cmdletbinding()]
 param (
     [parameter(Mandatory = $false)]
-    [System.IO.FileInfo]$ModulePath = "$PSScriptRoot\pwsh.frontmatter",
+    [System.IO.FileInfo]$ModulePath = "$PSScriptRoot/pwsh.frontmatter",
 
     [parameter(Mandatory = $false)]
     [switch]$BuildLocal,
@@ -15,25 +15,31 @@ try {
     $moduleName = Split-Path -Path $ModulePath -Leaf
     $PreviousVersion = Find-Module -Name $moduleName -ErrorAction SilentlyContinue | Select-Object *
     [Version]$exVer = $PreviousVersion ? $PreviousVersion.Version : $null
-    if ($CleanBuildDirectory -and $(Test-Path "$PSScriptRoot\bin\release\*")) {
+    if ($CleanBuildDirectory -and $(Test-Path "$PSScriptRoot/bin/release/*")) {
         Write-Host "Cleaning build directory.."
-        $null = Remove-Item -Path "$PSScriptRoot\bin\release\*" -Recurse -Force
+        $null = Remove-Item -Path "$PSScriptRoot/bin/release/*" -Recurse -Force
     }
     if ($BuildLocal) {
-        $rev = ((Get-ChildItem -Path "$PSScriptRoot\bin\release\" -ErrorAction SilentlyContinue).Name | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum) + 1
+        [int]$rev = ((Get-ChildItem -Path "$PSScriptRoot/bin/release/" -ErrorAction SilentlyContinue).Name | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum) + 1
         $newVersion = New-Object -TypeName Version -ArgumentList 1, 0, 0, $rev
     }
     else {
         $newVersion = if ($exVer) {
-            $rev = ($exVer.Revision + 1)
+            [int]$rev = ($exVer.Revision + 1)
             New-Object version -ArgumentList $exVer.Major, $exVer.Minor, $exVer.Build, $rev
         }
+        elseif (-not ([string]::IsNullOrEmpty($env:BUILD_BUILDID))) {
+            Write-Verbose 'We are in Azure DevOps it seems...'
+            [int]$rev = $env:BUILD_BUILDID
+            New-Object Version -ArgumentList 1, 0, 0, $rev
+        }
         else {
-            $rev = ((Get-ChildItem "$PSScriptRoot\bin\release\" -ErrorAction SilentlyContinue).Name | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum) + 1
+            Write-Verbose 'Assume GitHub'
+            [int]$rev = $env:GITHUB_RUN_NUMBER
             New-Object Version -ArgumentList 1, 0, 0, $rev
         }
     }
-    $releaseNotes = (Get-Content ".\$moduleName\releaseNotes.txt" -Raw -ErrorAction SilentlyContinue).Replace("{{NewVersion}}", $newVersion)
+    $releaseNotes = (Get-Content "$ModulePath/ReleaseNotes.txt" -Raw -ErrorAction SilentlyContinue).Replace("{{NewVersion}}", $newVersion)
     if ($PreviousVersion) {
         $releaseNotes = @"
 $releaseNotes
@@ -45,10 +51,10 @@ $($previousVersion.releaseNotes)
 
     #region Build out the release
     if ($BuildLocal) {
-        $relPath = "$PSScriptRoot\bin\release\$rev\$moduleName"
+        $relPath = "$PSScriptRoot/bin/release/$rev/$moduleName"
     }
     else {
-        $relPath = "$PSScriptRoot\bin\release\$moduleName"
+        $relPath = "$PSScriptRoot/bin/release/$moduleName"
     }
     "Version is $newVersion"
     "Module Path is $ModulePath"
@@ -58,13 +64,13 @@ $($previousVersion.releaseNotes)
         New-Item -Path $relPath -ItemType Directory -Force | Out-Null
     }
 
-    Copy-Item -Path "$ModulePath\*" -Destination "$relPath" -Recurse -Exclude ".gitKeep", "releaseNotes.txt", "description.txt"
+    Copy-Item -Path "$ModulePath/*" -Destination "$relPath" -Recurse -Exclude ".gitKeep", "releaseNotes.txt", "description.txt"
 
     $Manifest = @{
-        Path              = "$relPath\$moduleName.psd1"
+        Path              = "$relPath/$moduleName.psd1"
         ModuleVersion     = $newVersion
-        Description       = (Get-Content "$ModulePath\description.txt" -Raw).ToString()
-        FunctionsToExport = (Get-ChildItem -Path "$relPath\Public\*.ps1" -Recurse).BaseName
+        Description       = (Get-Content "$ModulePath/description.txt" -Raw).ToString()
+        FunctionsToExport = (Get-ChildItem -Path "$relPath/public/*.ps1" -Recurse).BaseName
         ReleaseNotes      = $releaseNotes
     }
     Update-ModuleManifest @Manifest
